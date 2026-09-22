e3fb339e5d52fac6396b10e3754576157c52ffd0
khanh-aera <khanh-aera@users.noreply.github.com>
2026-09-22 10:33:52 +0700

    khanh-aera - Add Support Ticket QR codes for arcade play
    - Generate QR codes for all 12 Support Tickets
    - Update ticket data to use new QR images
    - Copy QR images to site img/ directory
    - Bump SW to v5 to cache new images
    
    Now you can scan these QRs directly at the arcade to assign support!

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 /* Mezastar Binder service worker. Bump VERSION whenever the site changes. */
-const VERSION = "mezastar-v4";
+const VERSION = "mezastar-v5";
 const SHELL = ["./", "index.html", "assets/style.css", "assets/app.js",
   "data/roster.json", "data/bosses.json", "data/typechart.json", "data/pool.json",
   "manifest.webmanifest", "icons/icon-192.png", "icons/icon-512.png"];
